c5cd56d06190b3a58b1547847494860dcafb1b0c
sudipto <cg.sudipc@gmail.com>
2023-09-22 08:40:41 +0530

    Adapter added, changed item view layout to wrap-content

--- app/src/main/java/com/example/recyclerviewboiler1/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewboiler1/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.recyclerviewboiler1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewboiler1.datasource.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dataSet = Datasource().loadConstellations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = DataAdapter(this, dataSet)
+        recyclerView.setHasFixedSize(true)
+
+
     }
 }
